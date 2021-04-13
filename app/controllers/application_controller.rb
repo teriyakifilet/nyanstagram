e21268e36d5cda74d5fcd728badae82b3d6ffa53
teriyakifilet <oresamasaikooo@gmail.com>
2021-04-14 03:07:34 +0900

    新規登録・ログイン/アウトのリンク

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -11,5 +12,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :user_profile])
   end
 end
