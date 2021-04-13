2225301d3d244da9162ccfe85451355409f224c3
teriyakifilet <oresamasaikooo@gmail.com>
2021-04-14 03:06:43 +0900

    user関連アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cats, dependent: :destroy
+
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 32 }
+
+  # パスワードは半角英数字混合での入力が必須
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  
 end
