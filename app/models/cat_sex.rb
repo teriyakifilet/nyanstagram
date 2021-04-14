8504872ec164409218b12f6e8e230846422f41e9
teriyakifilet <oresamasaikooo@gmail.com>
2021-04-14 21:47:07 +0900

    ねこカテゴリ追加

---
+++ app/models/cat_sex.rb
@@ -0,0 +1,11 @@
+class CatSex < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'オス' },
+    { id: 3, name: 'メス' },
+    { id: 4, name: '不明' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :cats
+end
