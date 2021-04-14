391ab576a510ee58330a8efda88e3f534dc0e91f
teriyakifilet <oresamasaikooo@gmail.com>
2021-04-14 21:45:48 +0900

    catテーブルにuser_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_172541) do
+ActiveRecord::Schema.define(version: 2021_04_14_124104) do
 
   create_table "cats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "cat_name", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_04_10_172541) do
     t.integer "cat_breed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_cats_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_04_10_172541) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cats", "users"
 end
