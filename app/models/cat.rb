ba6601b5218c5247f7a6ff14379a6a3fd6ab4af4
teriyakifilet <oresamasaikooo@gmail.com>
2021-04-27 09:42:37 +0900

    cat-photoアソシエーションの再設定

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Cat < ApplicationRecord
+  has_many :cat_photos
+  has_many :photos, through: :cat_photos
   belongs_to :user
   has_one_attached :icon
-  has_many :photos, through: :cat_photos
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :cat_sex
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
-  has_one_attached :cat_photo
+  has_many :cat_photos
   has_many :cats, through: :cat_photos
+  has_one_attached :cat_photo
 end
