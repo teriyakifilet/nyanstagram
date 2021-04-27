2b3fa75909ffe74de7f9f8eabb410eb4e4ec8872
teriyakifilet <oresamasaikooo@gmail.com>
2021-04-27 13:05:48 +0900

    photoバリデーションの設定

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,8 @@ class Photo < ApplicationRecord
   has_many :cat_photos
   has_many :cats, through: :cat_photos
   has_one_attached :cat_photo
+
+  with_options presence: true do
+    validates :cat_ids, :cat_photo
+  end
 end
