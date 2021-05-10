fcb616177e1742155d1c732e2c82cf7b52a52162
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-10 17:59:44 +0900

    写真情報編集ページ　サーバー側実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!, except: [:show]
+  before_action :set_photo, only: [:show, :edit, :update]
 
   def new
     @photo = Photo.new
@@ -14,18 +15,26 @@ class PhotosController < ApplicationController
   end
 
   def show
-    @photo = Photo.includes(:cats, :user).find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @photo.update(photo_params)
+      redirect_to photo_path
+    else
+      render :edit
+    end
   end
-  
+
   private
 
   def photo_params
     params.require(:photo).permit(:cat_photo, :detail, cat_ids: []).merge(user_id: current_user.id)
+  end
+
+  def set_photo
+    @photo = Photo.includes(:cats, :user).find(params[:id])
   end
 end
