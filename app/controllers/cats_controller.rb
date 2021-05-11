5eddec2f185afd0f0367c1d3c989a80cd8ebdd5c
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-12 02:16:12 +0900

    ねこ情報編集ページ サーバーサイド実装

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -25,9 +25,15 @@ class CatsController < ApplicationController
   end
 
   def edit
+    @cat = Cat.includes(:user).find(params[:id])
   end
 
   def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_path
+    else render :edit
+    end
   end
 
   private
