00f2bff1062f5d3a03a735fa5e519b96ac60cf77
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-12 02:30:19 +0900

    ユーザー確認機能追加

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,6 +2,8 @@
 
 class CatsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
+  before_action :set_cat, only: %i[show edit update]
+  before_action :user_id_verification, only: %i[edit update]
 
   def index
     @photos = Photo.includes(:cats).order('created_at DESC').limit(5)
@@ -20,16 +22,13 @@ class CatsController < ApplicationController
   end
 
   def show
-    @cat = Cat.includes(:user, :photos).find(params[:id])
     @photos = @cat.photos
   end
 
   def edit
-    @cat = Cat.includes(:user).find(params[:id])
   end
 
   def update
-    @cat = Cat.find(params[:id])
     if @cat.update(cat_params)
       redirect_to cat_path
     else render :edit
@@ -40,5 +39,13 @@ class CatsController < ApplicationController
 
   def cat_params
     params.require(:cat).permit(:cat_name, :cat_age, :cat_sex_id, :cat_breed_id, :icon).merge(user_id: current_user.id)
+  end
+
+  def set_cat
+    @cat = Cat.includes(:user, :photos).find(params[:id])
+  end
+
+  def user_id_verification
+    redirect_to root_path unless current_user.id == @cat.user_id
   end
 end
