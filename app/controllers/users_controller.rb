358f7e69c1b662a96ea3df4095650bba7f9cb7ad
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-13 19:26:14 +0900

    ユーザー退会機能 サーバー側実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user
+
   def show
-    @user = User.includes(:cats, :photos).find(params[:id])
     @cats = @user.cats
     @photos = @user.photos
+  end
+
+  def destroy
+    redirect_to root_path if @user.destroy
+  end
+
+  private
+  def set_user
+    @user = User.includes(:cats, :photos).find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cats#index'
-  resources :users, only: %i[show]
+  resources :users, only: %i[show destroy]
   resources :cats, only: %i[new create show edit update destroy]
   resources :photos, only: %i[new create show edit update destroy]
 end
