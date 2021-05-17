7bf73974d9ae22b42f7d0a23019f24e2aca2e965
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-18 03:48:57 +0900

    Relationshipモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_170654) do
+ActiveRecord::Schema.define(version: 2021_05_17_184550) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,16 @@ ActiveRecord::Schema.define(version: 2021_05_15_170654) do
     t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "cat_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cat_id"], name: "index_relationships_on_cat_id"
+    t.index ["user_id", "cat_id"], name: "index_relationships_on_user_id_and_cat_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -91,4 +101,6 @@ ActiveRecord::Schema.define(version: 2021_05_15_170654) do
   add_foreign_key "likes", "photos"
   add_foreign_key "likes", "users"
   add_foreign_key "photos", "users"
+  add_foreign_key "relationships", "cats"
+  add_foreign_key "relationships", "users"
 end
