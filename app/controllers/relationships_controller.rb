bf76df3ee9dea19fcace0d1f43ea15f56e1e8da7
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-18 06:31:54 +0900

    フォロー機能 サーバー側実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,25 @@
 class RelationshipsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_cat
+  before_action :set_follow
+
+  def create
+    follow = Relationship.create(user_id: current_user.id, cat_id: @cat.id)
+  end
+
+  def destroy
+    follow = Relationship.find_by(user_id: current_user, cat_id: @cat.id)
+    follow.delete
+  end
+
+  private
+
+  def set_cat
+    @cat = Cat.find(params[:cat_id])
+  end
+
+  def set_follow
+    cat = Cat.find(params[:cat_id])
+  end
 end
