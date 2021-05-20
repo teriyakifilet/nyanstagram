42b45650c21f4fe334239d3a507b835c3ae44276
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-21 05:05:15 +0900

    Bootstrap導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 //= require jquery
 //= require rails-ujs
+import 'bootstrap'
+import 'bootstrap/scss/bootstrap.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
