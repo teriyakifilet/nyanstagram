2892b4c933f8ca2e9968074232301e2aa0d50207
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-20 10:12:32 +0900

    フォロー一覧表示機能 サーバー側実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,6 +4,10 @@ class RelationshipsController < ApplicationController
   before_action :set_cat, except: [:index]
   before_action :set_follow, except: [:index]
 
+  def index
+    @user = User.includes(:following).find(params[:user_id])
+  end
+
   def create
     follow = Relationship.create(user_id: current_user.id, cat_id: @cat.id)
   end
