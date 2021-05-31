2c0bb5e8dc96c0f4d30cc3ad611d931543e05707
teriyakifilet <oresamasaikooo@gmail.com>
2021-05-31 18:36:56 +0900

    HerokuでのBootstrap適用

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 //= require jquery
 //= require rails-ujs
 import 'bootstrap'
+import '../stylesheets/application'
 import 'bootstrap/scss/bootstrap.scss'
 import '@fortawesome/fontawesome-free/js/all'
 // Uncomment to copy all static images under ../images to the output folder and reference
